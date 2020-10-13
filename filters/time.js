e484863313d3f8b94f91ab249467102a60651431
Tom Hermans <tomhermans@gmail.com>
2020-10-13 23:17:23 +0200

    fiddling with time

--- filters/time.js
+++ filters/time.js
@@ -5,7 +5,7 @@ module.exports = function (data) {
 	let locale = data.locale || "en-US";
 	let now = new Date();
 	now.setTime(now.getTime() + 30000); // the build takes about 30 seconds... let's add a little buffer.
-	console.log(now);
+	console.log(now.getHours(), now.getMinutes());
 	return (time = now.toLocaleString(locale, {
 		timeZone: data.location,
 		hour: "numeric",
